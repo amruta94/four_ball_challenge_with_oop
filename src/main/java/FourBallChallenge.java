759a078bed05c0765382c49e9a46b724325a2179
Amruta Gogawale <abgogawale@gmail.com>
2021-11-19 10:37:06 +0530

    Created Frame

---
+++ src/main/java/FourBallChallenge.java
@@ -0,0 +1,17 @@
+import processing.core.PApplet;
+
+public class FourBallChallenge extends PApplet {
+
+    public static final int WIDTH_OF_FRAME = 640;
+    public static final int HEIGHT_OF_FRAME = 480;
+
+    public static void main(String[] args) {
+        PApplet.main("FourBallChallenge",args);
+    }
+
+    @Override
+    public void settings() {
+        super.settings();
+        size(WIDTH_OF_FRAME, HEIGHT_OF_FRAME);
+    }
+}
